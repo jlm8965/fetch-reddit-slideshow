03ec6fe96fa018f440b9ea50e58107d4e3ec35ed
jlm8965 <JulieLMonsees@gmail.com>
2022-09-27 08:18:22 -0500

    additinal changes made

---
+++ js/Solution.js
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Hello Reddit</title>
+</head>
+<body>
+  <h1>Reddit Slideshow</h1>
+
+  <form id="search-form">
+    <div>
+      <label>What you want?</label>
+      <input id="query" type="text" placeholder="Search on Reddit">
+    </div>
+    <input type="submit" value="GO LOOK!">
+  </form>
+
+  <hr>
+
+  <div>
+    <button id="stop">STOOOOOOOOOOOOOOOP</button>
+    <h2 id="title">Slideshow!</h2>
+    <div id="slideshow"></div>
+    <h4>Originally posted in <a id="subreddit"></a>!</h4>
+  </div>
+
+  <!-- <script src="./js/app.js"></script>
+</body>
+</html>
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status -->
--- js/index.js
+++ js/index.js
@@ -1,68 +1,71 @@
 //images array
 let currPhotoArr =  []
+const API_URL = 'https://www.reddit.com/search.json?limit=15&q='
+const INTERVAL_DELAY = 2000
+const currentIndex = 0
+const interval = 2000
 
-const currPhoto = document.querySelector('photobox')
-const photobox = document.querySelector('#photo')
-const stopButton = document.querySelector('#stop')
-const searchButton = document.querySelector('#search')
-const nextButton = document.querySelector('#next')
+//forcing JS to wait to run its code until DOM has been loaded
+document.addEventListener('DOMContentLoaded', function(){
 
-const form = document.querySelector('#form')
+//event listeners to buttons
+//search to search Reddit on click 
+document.getElementById(searchButton).addEventListener('submit', fetchFromReddit )
+//stop button to stop slides on click
+document.getElementById(stopButton).addEventListener('click', stopSlides)
+})
+//event listener to next button to move to next photo
+document.getElementById(nextButton).addEventListener('click', nextPhoto)
 
-const onGetPhotoSuccess = (photoArray) => {
-    console.log(photoArray)
-    const levelTwo = photo.Array.data
-    levelTwo.children.forEach(photo => {
-        currPhotoArr.push(photo.data.thumbnail)
-        const reddit = document.createElement('img')
-        reddit.classList.add('reddit')
-        reddit.setAttribute('src', photo.data.thumbnail)
-        photoBucket.setAttribute('alt', 'an image of /constname/ from reddit')
-    })
-    photoAdvance()
+//functions
+//function fetch photos from Reddit
+function fetchFromReddit(e) {
+  //asking to not refresh the page
+  e.preventDefault()
 }
 
-console.log('test', currPhotoArr)
-//create Element. input value needs to be tied to click. 
+//grabbing value from search box to search on Reddit
+const q = document.getElementById('query');
+if(query) {
+  fetch(API_URL + query)
+  .then(function(responseObj){
+    return responseObj.json()
+  })
+  .then(function(jsonResults){
+    const results = jsonResults.data.children
+  })
+}
 
-// imagesArray[0]= {
-// image:whiteKitty (), 
-// src: "https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg", "WhiteKitty"
-// caption: "WhiteKitty"
-// }
+  //showImage
+const showImage = (image) => {
+    //getter method - getAttribute
+    const imageUrl = image.target.getAttribute('https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg')
+    fetch(imageUrl)
+    //have to return as JSON
+    .then(res => res.json())
+    .then(showImageSuccess)
+    .catch(console.error)
 
-// //imagefactory
-// const createImage = creatingImageArray(https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg, whiteKitty)
-// const img = newImage(); 
-// img.src = "https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg"
-// img.alt = "White Kitty"
-// img.title = "White Kitty"
-// return img;
-// }
-
-
-//pushing two images to the array
-// image.push(createImage("https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg", "White Kitty"))
-// image.push(createImage("https://a.thumbs.redditmedia.com/u2P9aNDI8BL7apOH7QfJUYjWR6h1P59JtB4Hf9kdzi8.jpg", "Black and White Kitty"))
-// console.log(image); 
 
 //array of images to be looped through in slideshow
-// const imagesArray = (imagesArray) => {
-//     imagesArray.results.forEach(image => {
-//         //creating div for each image
-//         const imageSpace = document.createElement('div')
-//         //naming image div
-//         imageSpace.imageOneSpace = image.name
-//         //add a class to each div
-//         imageSpace.classList.add('imageClass')
-//         //setter-method
-//         imageSpace.setAttribute(img src="https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg", imageUrl)
-//         //click event
-//         imageSpace.addEventListener('click', moveImageForward)
-//         //take image and add to the container
-//         container.appendChild(image)
-//     })
-// }
+const imagesArray = (image) => {
+    imagesArray.results.forEach(image => {
+        //creating div for each image
+        const imageSpace = document.createElement('div')
+        //naming image div
+        imageSpace.imageOneSpace = image.name
+        //add a class to each div
+        imageSpace.classList.add('imageClass')
+        //giving img tag an id
+        document.getElementById("imageid").src'https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg')
+        //setter-method
+        imageSpace.setAttribute(img src=URL('https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg'))
+        //click event
+        imageSpace.addEventListener('click', moveImageForward)
+        //take image and add to the container
+        container.appendChild(image)
+    })
+}
 
 const photoAdvance = (currPhotoArr) => {
     currPhotoIndex++
@@ -70,6 +73,7 @@ const photoAdvance = (currPhotoArr) => {
         currPhotoIndex = 0
     }
 }
+
 
 const getCurrImage = document.querySelector('.photobox')
 console.log(getCurrImage)
@@ -80,57 +84,20 @@ reddit.setAttribute('src', currPhotoArr[currPhotoIndex])
 reddit.setAttribute('alt', `this is an image of ${constname} from reddit`)
 reddit.appendChild(reddit)
 
-// const showImageSuccess = (image) => {
-//     container.getElementsByClassName.display = 'none'
-//     const 
-// }
 
 //images slideshow
-const currphotoArray = document.querySelectorAll("img");
+const currphotoArr = document.querySelectorAll("img");
   let i = 0;
     setInterval(function(){ 
     if(i == 0) {
-      currphotoArray[i].className = "fade-in-image";
+      currphotoArr[i].className = "fade-in-image";
     } else if(i == photoArray.length ) {
-      currphotoArray[i - 1].className = "fade-out-image";
-      currphotoArray[0].className = "fade-in-image";
+      currphotoArr[i - 1].className = "fade-out-image";
+      currphotoArr[0].className = "fade-in-image";
       i = 0;
     } else {
-      currphotoArray[i - 1].className = "fade-out-image";
-      currphotoArray[i].className = "fade-in-image";
+      currphotoArr[i - 1].className = "fade-out-image";
+      currphotoArr[i].className = "fade-in-image";
     }
     i++;
   }, 2000);
-
-//   //showImage
-// const showImage = (image) => {
-//     //getter method - getAttribute
-//     const imageUrl = image.target.getAttribute('https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg')
-//     //console.log(pokeUrl)
-//     fetch(imageUrl)
-//     //have to return as JSON
-//     .then(res => res.json())
-//     .then(showImageSuccess) //formerly onShowPokemonSuccess
-//     .catch(console.error)
-
- searchButton.addEventListener('searchClick', () => {
-     fetch('https://b.thumbs.redditmedia.com/1BCF9_RhHGW6ey65N4kIeeFZJDoINM7I0idajXV42lo.jpg')
-     .then(res => res.json())
-     .then(onGetPhotoSuccess)
-     .catch(console.error)
- })
-stopButton.addEventListener('stopClick', submitted);
-nextButton.addEventListener('nextClick', submitted);
-
-// //functional search bar
-// const f = document.getElementById('form');
-// const q = document.getElementById('query');
-// const reddit = 'https://www.reddit.com/';
-// const site = 'reddit.com';
-
-// function submitted(event) {
-//   event.preventDefault();
-//   const url = reddit + site + '+' + q.value;
-//   const win = window.open(url, '_blank');
-//   win.focus();
-// }
